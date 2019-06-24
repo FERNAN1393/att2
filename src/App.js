28825d818a625631b0e1d7d8d4c526e7d6bfcb46
lalovar <edvarluc@gmail.com>
2019-06-24 19:32:56 +0000

    Forgot pass changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+/**
+*   Description: Main app Container. Routes for app.
+**/
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-          Learn React
-    </div>
-  );
+//  ----------- TBDeleted -------------------
+import ForgotPass from './containers/ForgotPass';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+//  -----------------------------------------
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forgotPassModal: false
+    };
+  }
+  
+  toggleForgotModal = () => {
+    this.setState(prevState => ({
+      forgotPassModal: !prevState.forgotPassModal
+    }));
+  }
+  
+  render() {
+    return (
+      <div>
+        Learn React
+        
+        <br />
+        <Button color="danger" onClick={()=>{this.toggleForgotModal()}}>toggle forgot modal</Button>
+        <Modal isOpen={this.state.forgotPassModal} fade={false} toggle={this.toggleForgotModal} >
+          <ForgotPass toggleFun={this.toggleForgotModal}/>
+          <ModalFooter>
+          <p>
+            <b>Need help?</b> If you are not longer using this SapId, please contact your manager.
+          </p>
+          </ModalFooter>
+        </Modal>
+        
+        <br />
+        
+      </div>
+    )
+    
+  };
 }
 
 export default App;
--- src/containers/ForgotPass.jsx
+++ src/containers/ForgotPass.jsx
@@ -9,36 +9,43 @@ import {checksapIdAction} from './../redux/actions/action.forgotPass';
 // loading image
 import loading_image from './../resources/img/blue_loading.gif'; // Tell Webpack this JS file uses this image
 // Reactstrap components
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
-
+import { Button, Input, ModalHeader, ModalBody } from 'reactstrap';
 
 class ForgotPass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       sapId: '12345678',
-      stage: 1
+      stage: 1,
+      
     };
   }
   
+  printRedux = async () =>{
+    await this.props.checksapIdAction(this.state.sapId);
+    console.log(this.props.forgotObj)
+  }
+  
   handleChange = event => {
-    this.setState({sapId: event.target.value});
+    this.setState({[event.name]: event.target.value});
   }
   
-  handleSubmit = event => {
-  alert('A name was submitted: ' + this.state.sapId);
-  event.preventDefault();
+  handleSubmit = () => {
+    this.printRedux();
   }
   
-  toggle = async () => {
-    console.log("first print",this.props.forgotObj)
-    console.log("after hitting accion, printing payload", 
-      await this.props.checksapIdAction(this.state.sapId).payload)
-    console.log("new redux state", this.props.forgotObj)
+  
+  setButtonText = () => {
+    if ( this.state.stage === 1)
+      return "Send SapId "
+    if ( this.state.stage === 2)
+      return "Send Aswers "
+    if ( this.state.stage === 3)
+      return "Change Password "
   }
   
   render() {
+    const buttonText = this.setButtonText();
     return (
       <div>
           <ModalHeader toggle={this.props.toggle}>Reset your password</ModalHeader>
@@ -47,16 +54,26 @@ class ForgotPass extends React.Component {
               Forgot Password?
             </h1>
             <p>
-              No problem! Enter your SapId you use to sign in. Next aswers your security questions.
+              No problem! Enter your SapId you use to sign in. Next aswer your security questions.
             </p>
-            <h5>
+            <p>
               <b>SapId</b>
-            </h5>
+            </p>
+            <Input name="sapId" id="inputSapId" placeholder="Enter SapId here!" onChange={evt=>{this.handleChange(evt)}}/>
             
+            <center>
+            <br />
+              <Button color="secondary" size="md" block onClick={()=>{this.handleSubmit()}}>
+                {buttonText}
+              </Button>{' '}
+            </center>
+            
+            <img src={loading_image} alt="loading..." />
+            
+            <center>
+              <Button color="link" size="md" block onClick={()=>{this.props.toggleFun()}}>Cancel</Button>
+            </center>
           </ModalBody>
-          <ModalFooter>
-            <Button color="secondary"  onClick={()=>this.toggle()}>Cancel</Button>
-          </ModalFooter>
       </div>
     );
   }
