9544a352cc6b957f412073bc73ba4b1cebf3eb58
lalovar <edvarluc@gmail.com>
2019-06-26 14:53:17 +0000

    Mergin issues in ForgotPass

--- src/controllers/ctrl.ForgotPass.js
+++ src/controllers/ctrl.ForgotPass.js
@@ -9,11 +9,7 @@ const output = {
 
 //  chkSapId function controller
 //  input:  sapId
-<<<<<<< HEAD
-//  output: Object with questions
-=======
 //  output: Security questions(Obj)
->>>>>>> dev-frank
 export const chkSapId = passObj => {
     if( passObj == "12345678" )
         return {
