1728a3f1122b3bc56871bf28597f35733a4b9199
Ubuntu <ubuntu@ip-172-31-19-118.us-east-2.compute.internal>
2019-06-26 14:08:31 +0000

    minpor fixes on signin fom

--- src/redux/actions/action.signIn.js
+++ src/redux/actions/action.signIn.js
@@ -9,7 +9,7 @@ export const signInUser = ({email, psw }) =>  async dispatch => {
     try {
         const userLoggedInfo = await SignIn(email, psw);
         userLoggedInfo && dispatch({type: USER_LOGGED, user: userLoggedInfo })
-        return {success:true,errorMessage:  !userLoggedInfo && 'User invalid, email or password is incorrect!'};
+        return {success: !!userLoggedInfo, errorMessage:  !userLoggedInfo && 'User invalid, email or password is incorrect!'};
     }catch(e){
         return {success:false, errorMessage: 'Verify your internet connection'}
     }
