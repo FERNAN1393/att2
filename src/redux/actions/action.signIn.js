12ebf1caf4eaf1b806385f975382f10cbac3da95
lalovar <edvarluc@gmail.com>
2019-06-28 20:37:37 +0000

    add createAccount component

--- src/redux/actions/action.signIn.js
+++ src/redux/actions/action.signIn.js
@@ -1,7 +1,5 @@
-
 import { SignIn }  from '../../controllers/ctrl.SignInForm';
 import { USER_LOGGED } from './../../constants/reduxActions';
-
 /*
 Params: User : { }
 */
@@ -15,42 +13,4 @@ export const signInUser = ({email, psw }) =>  async dispatch => {
         console.log('Error');
         return {success:false, errorMessage: 'Verify your internet connection'};
     }
-
 };
-
-const user = {
-  projectName: 'Attendance ',
-  projectCode: '001',
-  reportingManager: '12345678',
-  sapId: '99999999',
-  batchNumber: '000',
-  employeeName: 'Admin',
-  email: 'admin@hcl.com',
-  password: 'admin',
-  role: '1',
-  client: 'HCL',
-  workLocation: 'Vista Acueducto',
-  employeeStatus: 'Local-nativo',
-  secureQuestions: [
-    'What is your favorite movie?',
-    'What is your pet’s name?'
-  ],
-  secureAnswers: [
-    'attendance',
-    'attendance'
-  ],
-  status: 'active'
-};
-
-// const user = {
-//   projectName: 'USAA EIS MCA Mexico',
-//   projectCode: 'C/140685',
-//   reportingManager: '51477851',
-//   sapId: '51643140',
-//   batchNumber: '302',
-//   employeeName: 'SIMON PEDRO GUERRERO AGUILAR',
-//   email: 'Simon.Aguilar@hcl.com',
-//   role: '2',
-//   client: 'USAA',
-//   status: 'active'
-// };
